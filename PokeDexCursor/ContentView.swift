0fdc6b840c54ad506178b7df0bbc227ea4540a2c
yuya-3110 <ysuu45143745@gmail.com>
2025-03-18 22:51:42 +0900

    Initial Commit

---
+++ PokeDexCursor/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  PokeDexCursor
+//  
+//  Created by Yuchinante on 2025/03/18
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PokeDexCursor/PokeDexCursorApp.swift
@@ -0,0 +1,18 @@
+//
+//  PokeDexCursorApp.swift
+//  PokeDexCursor
+//  
+//  Created by Yuchinante on 2025/03/18
+//  
+//
+
+import SwiftUI
+
+@main
+struct PokeDexCursorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
